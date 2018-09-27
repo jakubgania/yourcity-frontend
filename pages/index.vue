c5d062df92077368eff7ed01e0b3ff6c0a33e222
jakubgania <jakubgania@outlook.com>
2018-09-27 23:34:49 +0200

    home page - components vue and scss/home folder created

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,35 @@
 <template>
-  <v-layout>
-    start yourcity project
+  <v-layout
+    row
+    wrap
+  >
+    <v-flex
+      xs12
+      lg12
+    >
+      <!-- search component -->
+      <!-- informations descriptions component -->
+      <!-- image -->
+      <!-- categories component -->
+      <!-- image -->
+      <!-- tags component -->
+      <!-- image -->
+      <!-- gallery component -->
+      <!-- image -->
+      <!-- cities component -->
+      <!-- image -->
+      <!-- business information component -->
+      <!-- map component -->
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
+export default {
+  head() {
+    return {
+      title: 'YOURCITY - Home page',
+    };
+  },
+};
 </script>
