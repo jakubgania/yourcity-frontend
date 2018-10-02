a91a95bc55f04924c13970c559d22041e7cc8dd6
jakubgania <jakubgania@outlook.com>
2018-10-02 23:58:11 +0200

    store/modules/profiles-search/getters.js

--- store/modules/profiles-search/getters.js
+++ store/modules/profiles-search/getters.js
@@ -1,45 +1,45 @@
-// const city = state => state.city;
-// const query = state => state.query;
-// const category = state => state.category;
-// const currentCategory = state => state.currentCategory;
-// const pagingURL = state => state.pagingURL;
-// const items = state => state.items;
-// const result = state => state.result;
-// const resultCopy = state => state.resultCopy;
-// const profilePosts = state => state.profilePosts;
-// const showModalImage = state => state.showModalImage;
-// const showLoader = state => state.showLoader;
-// const showTagSection = state => state.showTagSection;
-// const showPaginigButton = state => state.showPaginigButton;
-// const showProfileDetailsDialog = state => state.showProfileDetailsDialog;
-// const showProfileDetailsDialogMobile = state => state.showProfileDetailsDialogMobile;
-// const showPagingButtonLoader = state => state.showPagingButtonLoader;
-// const postLoaderButton = state => state.postLoaderButton;
-// const showEmptyResultsAlert = state => state.showEmptyResultsAlert;
-// const showErrorConnectionAlert = state => state.showErrorConnectionAlert;
-// const proposedTags = state => state.proposedTags;
-// const numberOfTags = state => state.numberOfTags;
-//
-// export {
-//   city,
-//   query,
-//   category,
-//   currentCategory,
-//   pagingURL,
-//   items,
-//   result,
-//   resultCopy,
-//   profilePosts,
-//   showModalImage,
-//   showLoader,
-//   showTagSection,
-//   showPaginigButton,
-//   showProfileDetailsDialog,
-//   showProfileDetailsDialogMobile,
-//   showPagingButtonLoader,
-//   postLoaderButton,
-//   showEmptyResultsAlert,
-//   showErrorConnectionAlert,
-//   proposedTags,
-//   numberOfTags,
-// };
+const city = state => state.city;
+const query = state => state.query;
+const category = state => state.category;
+const currentCategory = state => state.currentCategory;
+const pagingURL = state => state.pagingURL;
+const items = state => state.items;
+const result = state => state.result;
+const resultCopy = state => state.resultCopy;
+const profilePosts = state => state.profilePosts;
+const showModalImage = state => state.showModalImage;
+const showLoader = state => state.showLoader;
+const showTagSection = state => state.showTagSection;
+const showPaginigButton = state => state.showPaginigButton;
+const showProfileDetailsDialog = state => state.showProfileDetailsDialog;
+const showProfileDetailsDialogMobile = state => state.showProfileDetailsDialogMobile;
+const showPagingButtonLoader = state => state.showPagingButtonLoader;
+const postLoaderButton = state => state.postLoaderButton;
+const showEmptyResultsAlert = state => state.showEmptyResultsAlert;
+const showErrorConnectionAlert = state => state.showErrorConnectionAlert;
+const proposedTags = state => state.proposedTags;
+const numberOfTags = state => state.numberOfTags;
+
+export {
+  city,
+  query,
+  category,
+  currentCategory,
+  pagingURL,
+  items,
+  result,
+  resultCopy,
+  profilePosts,
+  showModalImage,
+  showLoader,
+  showTagSection,
+  showPaginigButton,
+  showProfileDetailsDialog,
+  showProfileDetailsDialogMobile,
+  showPagingButtonLoader,
+  postLoaderButton,
+  showEmptyResultsAlert,
+  showErrorConnectionAlert,
+  proposedTags,
+  numberOfTags,
+};
