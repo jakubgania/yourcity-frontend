f1a163cd78872adc9620a26812b67ee5c484fac5
jakubgania <jakubgania@outlook.com>
2018-10-06 01:36:09 +0200

    components/home/map

--- components/home/map.vue
+++ components/home/map.vue
@@ -1,5 +1,54 @@
 <template>
-  <div class="">
-    map component
-  </div>
+  <v-flex
+    xs12
+    lg12>
+    <v-layout
+      row
+      wrap
+      class="map-image-section"
+    >
+      <v-flex
+        xs12
+        lg8
+        offset-lg2
+      >
+        <img
+          :src="worldImage"
+          alt="World map"
+          style="width:100%;"
+          data-aos="fade"
+          data-aos-delay="400"
+          data-aos-once="true"
+        >
+        <div class="text-section">
+          <div
+            data-aos="fade"
+            data-aos-delay="800"
+            data-aos-once="true"
+          >
+            <div class="text">
+              Search for anything. Wherever you are.
+            </div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-flex>
 </template>
+
+<script>
+import worldImage from '../../assets/images/world.png';
+
+export default {
+  data() {
+    return {
+      worldImage,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/scss/home/map/lg.scss';
+  @import '../../assets/scss/home/map/xs.scss';
+</style>
