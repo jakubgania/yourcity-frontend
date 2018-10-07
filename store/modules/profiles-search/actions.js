4323742b6927039f52e1e03034bddb8f17406593
jakubgania <jakubgania@outlook.com>
2018-10-07 12:59:49 +0200

    store/modules/profiles-search/actions

--- store/modules/profiles-search/actions.js
+++ store/modules/profiles-search/actions.js
@@ -1,6 +1,39 @@
 // import axios from 'axios';
-// // import state from './state';
-//
+import state from './state';
+
+const updateResult = ({ commit }, value) => {
+  commit('updateResult', value);
+};
+
+const updateCategory = ({ commit }, value) => {
+  commit('switchCategory', value);
+};
+
+const updateCurrentCategory = ({ commit }, currentCategory) => {
+  commit('currentCategory', currentCategory);
+};
+
+const resetResult = ({ commit }) => {
+  commit('resetResult', state.resultCopy);
+};
+
+const queryUpdate = ({ commit }, query) => {
+  commit('query', query);
+};
+
+const cityUpdate = ({ commit }, city) => {
+  commit('city', city);
+};
+
+const switchCategory = ({ dispatch }, parameters) => {
+  const item = {
+    category: parameters.category,
+    city: parameters.city,
+  };
+
+  dispatch('getProfilesData', item);
+};
+
 // const getProfilesData = ({ commit, dispatch }, queryStringParameters) => {
 //
 //   // function to check if it exists category in queryStringParameters
@@ -12,7 +45,14 @@
 //       // error
 //     });
 // };
-//
-// export default {
-//   getProfilesData,
-// };
+
+export default {
+  updateResult,
+  updateCategory,
+  updateCurrentCategory,
+  resetResult,
+  queryUpdate,
+  cityUpdate,
+  switchCategory,
+  // getProfilesData,
+};
