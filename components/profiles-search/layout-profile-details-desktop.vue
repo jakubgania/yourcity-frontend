6401f9219c4fbd3a330c109f3be5ab2c66b16ddc
jakubgania <jakubgania@outlook.com>
2018-10-15 18:27:46 +0200

    update components/profiles-search/layout-profile-details-desktop

--- components/profiles-search/layout-profile-details-desktop.vue
+++ components/profiles-search/layout-profile-details-desktop.vue
@@ -39,6 +39,99 @@
           Opis
         </v-card-title>
 
+        <v-card-text
+          v-if="profileDetails.description"
+          :class="[ showFullDescription ? 'show-content' : '', 'hide-content']"
+          class="description-section"
+        >
+          {{ profileDetails.description }}
+        </v-card-text>
+        <v-card-text
+          v-else
+          class="section"
+        >
+          Brak
+        </v-card-text>
+        <div
+          v-if="showFullDescriptionButton"
+          class="full-description-button-section"
+        >
+          <v-layout
+            row
+            wrap
+          >
+            <v-flex
+              sm6
+              offset-sm3
+              md6
+              offset-md3
+              lg6
+              offset-lg3
+            >
+              <v-btn
+                block
+                depressed
+                class="full-description-button"
+                @click="showFullDescriptionEmit()"
+              >
+                {{ descriptionButtonText }}
+                <v-icon class="icon">{{ iconButtonText }}</v-icon>
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </div>
+
+        <v-card-title class="title-subsection">
+          <i class="material-icons icon">location_on</i> Adres
+        </v-card-title>
+
+        <v-card-text
+          v-if="profileDetails.single_line_address"
+          class="section">
+          {{ profileDetails.single_line_address }}
+          <div class="">
+            <div
+              v-if="profileDetails.location.street"
+              style="display:inline;"
+            >
+              <v-chip
+                outline
+                class="city-chip"
+              >
+                {{ profileDetails.location.street }}
+              </v-chip>
+            </div>
+            <div
+              v-if="profileDetails.location.city"
+              style="display:inline;"
+            >
+              <v-chip
+                outline
+                class="city-chip"
+              >
+                {{ profileDetails.location.city }}
+              </v-chip>
+            </div>
+            <div
+              v-if="profileDetails.location.country"
+              style="display:inline;"
+            >
+              <v-chip
+                outline
+                class="city-chip"
+              >
+                {{ profileDetails.location.country }}
+              </v-chip>
+            </div>
+          </div>
+        </v-card-text>
+        <v-card-text
+          v-else
+          class="section"
+        >
+          Brak
+        </v-card-text>
+
         <v-card-title class="title-subsection">
           <i class="material-icons icon">access_time</i>
           Godziny otwarcia
@@ -49,20 +142,117 @@
           Posty
         </v-card-title>
 
+        <div class="full-description-button-section">
+          <v-layout
+            row
+            wrap
+          >
+            <v-flex
+              sm6
+              offset-sm3
+              md6
+              offset-md3
+              lg6
+              offset-lg3
+            >
+              <v-btn
+                :loading="postLoaderButton"
+                :disabled="postLoaderButton"
+                block
+                depressed
+                class="full-description-button"
+                @click.native="$emit('loadProfilePosts', profileDetails.id)"
+              >
+                Pokaż wpisy
+                <v-icon class="icon">cloud_upload</v-icon>
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </div>
+
         <v-card-title class="title-subsection">
           <i class="material-icons icon">smartphone</i>
           Telefon
         </v-card-title>
+
+        <v-card-text
+          v-if="profileDetails.phone"
+          class="section"
+        >
+          {{ profileDetails.phone }}
+        </v-card-text>
+        <v-card-text
+          v-else
+          class="section"
+        >
+          Brak
+        </v-card-text>
 
         <v-card-title class="title-subsection">
           <i class="material-icons icon">language</i>
           Strona
         </v-card-title>
 
+        <v-card-text
+          v-if="profileDetails.website"
+          class="section"
+        >
+          <a
+            :href="profileDetails.website"
+            target="_blank"
+            style="text-decoration:none;"
+          >
+            <v-chip
+              outline
+              class="website-chip"
+            >
+              Przejdź do strony
+              <v-icon>keyboard_arrow_right</v-icon>
+            </v-chip>
+          </a>
+        </v-card-text>
+        <v-card-text
+          v-else
+          class="section"
+        >
+          Brak
+        </v-card-text>
+
         <v-card-title class="title-subsection">
           <i class="material-icons icon">share</i>
           Powiązane kategorie
         </v-card-title>
+
+        <div v-if="profileDetails.category_list">
+          <v-card-text class="category-list-section">
+            <div
+              v-for="category in profileDetails.category_list"
+              :key="category.id"
+              style="display:inline;"
+            >
+              <v-chip
+                outline
+                class="tag-chip"
+                @click.native="tag(category.name)"
+              >
+                {{ category.name }}
+              </v-chip>
+            </div>
+          </v-card-text>
+        </div>
+
+        <v-card-actions class="bottom-section">
+          <!-- powered by component -->
+          <v-spacer/>
+          <v-btn
+            color="blue accent-3"
+            flat
+            class="dialog-close-button"
+            @click.native="$emit('closeShowInformationDialog')"
+          >
+            zamknij
+          </v-btn>
+        </v-card-actions>
 
       </v-card>
     </v-dialog>
@@ -70,6 +260,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 // import { mapGetters, mapActions } from 'vuex';
 // posts component
 
@@ -102,7 +293,23 @@ export default {
     };
   },
   computed: {
-    //
+    ...mapGetters('searchProfiles', [
+      'postLoaderButton',
+    ]),
+    descriptionButtonText() {
+      if (this.showFullDescription) {
+        return 'Pokaż mniej';
+      }
+
+      return 'Pokaż więcej';
+    },
+    iconButtonText() {
+      if (this.showFullDescription) {
+        return 'keyboard_arrow_up';
+      }
+
+      return 'keyboard_arrow_down';
+    },
   },
   watch: {
     showInformationDialog(value) {
