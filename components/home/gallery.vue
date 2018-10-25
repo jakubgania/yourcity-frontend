c12faeee9667e508a1b65944dec8c9b46e276893
jakubgania <jakubgania@outlook.com>
2018-10-25 20:27:23 +0200

    updated components/home/gallery.vue

--- components/home/gallery.vue
+++ components/home/gallery.vue
@@ -1,5 +1,60 @@
 <template>
-  <div class="">
-    gallery component
-  </div>
+  <v-layout
+    row
+    wrap
+  >
+    <v-flex
+      xs10
+      offset-xs1
+      lg11
+      offset-lg1
+    >
+      <div
+        class="title-section"
+        data-aos="fade-right"
+        data-aos-delay="240"
+        data-aos-once="true"
+      >
+        Zdjęcia
+      </div>
+    </v-flex>
+
+    <!-- error alert -->
+
+    <!-- images grid -->
+
+    <!-- gallery button -->
+
+  </v-layout>
 </template>
+
+<script>
+// import { mapGetters, mapActions } from 'vuex'
+//
+// export default {
+//     data: function () {
+//         return {
+//         }
+//     },
+//     methods: {
+//     },
+//     computed: {
+//     },
+//     created() {
+//     }
+// }
+</script>
+
+<style lang="scss" scoped>
+.title-section
+{
+    font-size: 40px;
+    font-weight: 700;
+    letter-spacing: 2px;
+
+    padding-top: 100px;
+    padding-bottom: 100px;
+
+    color: #000000;
+}
+</style>
