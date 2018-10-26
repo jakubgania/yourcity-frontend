324d30877d7da807841cee3dd4894763203a8cb1
jakubgania <jakubgania@outlook.com>
2018-10-26 18:59:58 +0200

    implemented tags section on profiles-search

--- components/profiles-search/tags-section.vue
+++ components/profiles-search/tags-section.vue
@@ -1,10 +1,12 @@
 <template>
   <transition name="fade">
-    <div v-if="true">
+    <div
+      v-if="showTagSection && proposedTags.length > 0"
+      style="border-bottom: 2px solid #ced8e2;"
+    >
       <v-flex lg12>
         <div class="title-section">
-          <!-- {{ 'Tagi : ' + proposedTags.length }} -->
-          Tagi: 24
+          {{ 'Tagi : ' + proposedTags.length }}
         </div>
       </v-flex>
       <div class="tag-section">
@@ -12,11 +14,25 @@
           xs12
           lg12
         >
-          <!-- chip -->
+          <v-flex
+            v-for="index in numberOfTags"
+            :key="index.id"
+            xs12
+            lg12
+            style="display:inline;"
+          >
+            <v-chip
+              outline
+              class="tag-chip"
+              @click.native="$emit('searchByTag', proposedTags[index - 1])"
+            >
+              {{ proposedTags[index - 1] }}
+            </v-chip>
+          </v-flex>
         </v-flex>
       </div>
       <v-flex
-        v-if="false"
+        v-if="proposedTags.length > numberOfTags"
         lg4
         offset-lg4
         md6
@@ -26,19 +42,99 @@
         xs10
         offset-xs1
       >
-        <!-- button and dialog -->
+        <v-btn
+          large
+          block
+          depressed
+          color="white"
+          class="more-tags-button"
+          @click="showMoreTagsDialog(true)"
+        >
+          Więcej tagów
+          <v-icon class="icon">bubble_chart</v-icon>
+        </v-btn>
+
+        <v-dialog
+          v-model="showMoreTagsDialogModel"
+          max-width="600"
+          style="z-index:800;"
+          @keydown.esc="showMoreTagsDialog(false)"
+        >
+          <v-card>
+            <div class="top-title-section">
+              <v-icon class="icon-info">info_outline</v-icon>
+              Tagi
+              <v-icon
+                class="icon-close"
+                @click="showMoreTagsDialog(false)"
+              >
+                close
+              </v-icon>
+            </div>
+            <v-card-text>
+              <div
+                v-for="tag in proposedTags"
+                :key="tag.id"
+                class=""
+                style="display:inline;"
+              >
+                <v-chip
+                  outline
+                  class="tag-chip"
+                  @click.native="selectTag(tag)"
+                >
+                  {{ tag }}
+                </v-chip>
+              </div>
+            </v-card-text>
+            <v-card-actions class="bottom-section">
+              <v-spacer/>
+              <v-btn
+                color="blue accent-3"
+                style="font-size:12px;letter-spacing:2px;"
+                flat
+                @click.native="showMoreTagsDialog(false)"
+              >
+                Zamknij
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
       </v-flex>
-      tags component test
+
     </div>
   </transition>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+
 export default {
   data() {
     return {
-      //
+      showMoreTagsDialogModel: false,
     };
+  },
+  computed: {
+    ...mapGetters('searchProfiles', [
+      'city',
+      'showTagSection',
+      'proposedTags',
+      'numberOfTags',
+    ]),
+  },
+  methods: {
+    ...mapActions('searchProfiles', [
+      'updateShowTagSection',
+    ]),
+    showMoreTagsDialog(value) {
+      this.showMoreTagsDialogModel = value;
+    },
+    selectTag(tag) {
+      this.$emit('searchByTag', tag);
+      this.showMoreTagsDialog(false);
+    },
   },
 };
 </script>
