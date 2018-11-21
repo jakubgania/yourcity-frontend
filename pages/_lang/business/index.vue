9c4d047e555834b708efe88293f6504bcd482874
jakubgania <jakubgania@outlook.com>
2018-11-21 18:51:03 +0100

    business page - added description about yourcity

--- pages/_lang/business/index.vue
+++ pages/_lang/business/index.vue
@@ -2,6 +2,18 @@
 <template>
   <v-layout row wrap>
     <top-image-component/>
+
+    <v-layout row wrap class="about-description">
+      <v-flex lg6 offset-lg3>
+        <div class="example-profile-description" style="font-weight:700;text-align:center;">
+          YOURCITY ma pomagać ludziom odnaleźć interesujące ich informacje w danym mieście.
+          Ma być przydatnym narzędziem kiedy ktoś szuka konkretnego miejsca, jakieś usługi
+          albo siedziby Twojej firmy. YOURCITY to wysoka jakość informacji.
+        </div>
+      </v-flex>
+    </v-layout>
+
+
   </v-layout>
 </template>
 
@@ -30,3 +42,22 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.image-section
+{
+    width: 100%;
+    height: auto;
+}
+.about-description
+{
+  background-color: #ffffff;
+  margin-top: 140px;
+}
+.example-profile-description
+{
+    letter-spacing: 1px;
+    font-size: 16px;
+    margin-bottom: 60px;
+}
+</style>
