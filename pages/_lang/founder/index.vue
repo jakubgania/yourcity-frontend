a00913f928846401d7129b571bce887a702c9b6e
jakubgania <jakubgania@outlook.com>
2018-11-24 23:53:43 +0100

    implemented founder page

--- pages/_lang/founder/index.vue
+++ pages/_lang/founder/index.vue
@@ -1,20 +1,60 @@
+<!-- eslint-disable vue/max-attributes-per-line -->
 <template>
-  <v-layout
-    row
-    wrap
-    class="container-founder"
-  >
-    <v-flex
-      lg6
-      offset-lg3
-    >
-      O twórcy
+  <v-layout row wrap class="founder-container">
+    <v-flex xs12 lg4 offset-lg4>
+      <div
+        class="title-page"
+        data-aos="zoom-in"
+        data-aos-delay="200"
+        data-aos-once="true"
+      >
+        O twórcy
+      </div>
+    </v-flex>
+    <v-flex lg4 offset-lg4 style="text-align:center;">
+      <v-avatar
+        size="220"
+        color="grey lighten-4"
+        data-aos="zoom-in"
+        data-aos-delay="300"
+        data-aos-once="true"
+      >
+        <img :src="profileImg" alt="Jakub Gania - profile image">
+      </v-avatar>
+    </v-flex>
+    <v-flex lg4 offset-lg4 style="text-align:center;">
+      <div
+        class="headline"
+        data-aos="zoom-in"
+        data-aos-delay="400"
+        data-aos-once="true"
+      >
+        Jakub Gania
+      </div>
+      <div
+        class=""
+        style="margin-top:20px;"
+        data-aos="zoom-in"
+        data-aos-delay="400"
+        data-aos-once="true"
+      >
+        <a href="https://jakubgania.io" target="_blank" class="link">
+          jakubgania.io
+        </a>
+      </div>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import profileImg from '../../../assets/images/jakub-gania.jpg';
+
 export default {
+  data() {
+    return {
+      profileImg,
+    };
+  },
   head() {
     return {
       title: this.$t('founder.head.title'),
@@ -35,9 +75,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.container-founder
+.founder-container
 {
-  margin-top: 64px;
-  // background-color: #ffffff;
+  min-height: calc(100vh - 64px);
+  height: auto;
+  background-color: #ffffff;
+}
+.title-page
+{
+    font-size: 44px;
+
+    padding-top: 100px;
+    // padding-bottom: 100px;
+
+    text-align: center;
+    letter-spacing: 2px;
+
+    color: #111111;
+}
+.link
+{
+  text-decoration: none;
+  letter-spacing: 2px;
 }
 </style>
