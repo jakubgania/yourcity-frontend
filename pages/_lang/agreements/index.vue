36fd16868ee4a6ca0c00264570ee616b2cf88b33
jakubgania <jakubgania@outlook.com>
2018-11-28 19:21:34 +0100

    implemented agreements page

--- pages/_lang/agreements/index.vue
+++ pages/_lang/agreements/index.vue
@@ -1,20 +1,65 @@
+<!-- eslint-disable vue/max-attributes-per-line -->
 <template>
-  <v-layout
-    row
-    wrap
-    class="container-agreements"
-  >
+  <v-layout row wrap class="agreements-container">
+    <v-flex xs12 lg4 offset-lg4>
+      <div class="title-page">
+        Regulamin
+      </div>
+    </v-flex>
+
     <v-flex
-      lg6
-      offset-lg3
+      v-for="(rule, index) in rules"
+      :key="rule.id"
+      lg4
+      offset-lg4
+      style="font-size:18px;margin-top:30px;"
     >
-      Regulamin
+      <div v-html="index + 1 + '. ' + rule.message"/>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      rules: [
+        {
+          message: 'Użytkownik korzystający z aplikacji Yourcity akceptuje regulamin/postanowienia oraz zapoznał się ze wszystkimi postanowieniami zawartymi w regulaminie.',
+        },
+        {
+          message: 'Twórca aplikacji Yourcity nie bierze na siebie odpowiedzialności w związku z niepoprawnym działaniem strony lub nieaktualnymi lub nieprawidłowymi informacjami jakie są wyświetlane podczas wyszukiwania profili.',
+        },
+        {
+          message: 'Twórca aplikacji Yourcity nie bierze na siebie odpowiedzialności w związku z niepoprawnym działaniem strony lub nieaktualnymi lub nieprawidłowymi informacjami jakie są wyświetlane podczas wyszukiwania profili.',
+        },
+        {
+          message: 'Aplikacja Yourcity jest w ciągłej rozbudowie i może czasami działać niestabilnie lub w ogóle nie być dostępna dla użytkowników podczas aktualizacji.',
+        },
+        {
+          message: 'Źródłem danych dla aplikacji Yourcity jest <a href="https://developers.facebook.com/docs/places/web" target="_blank">Places Graph API - Facebook<a>.',
+        },
+        {
+          message: 'Aplikacja wykorzystuje pliki cookie jedynie w celach statystycznych.',
+        },
+        {
+          message: 'Aplikacja Yourcity może zostać w każdej chwili wyłączona przez osobę nią zarządzającą i przestać dostarczać informacje.',
+        },
+        {
+          message: 'Należy pamiętać, że informacje o profilach mogą być nieaktualne lub nieprawdziwe.',
+        },
+        {
+          message: 'Wszystkie zdjęcia na stronie głównej, stronie z galerią zdjęć oraz stronie z listą wszystkich karajów są własnością twórcy. Zabrania się kopiowania i nieuprawnionego wykorzystywania bez zgody autora.',
+        },
+        {
+          message: 'Twórca dokłada wszelkich starań by aplikacja działała jak najlepiej.',
+        },
+        {
+          message: 'W przypadku zaobserowania niestabilnej pracy aplikacji lub znalezienia błędów użytkonik proszony jest o kontakt z twórcą i opisanie problemu.',
+        },
+      ],
+    };
+  },
   head() {
     return {
       title: this.$t('agreements.head.title'),
@@ -35,9 +80,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.container-agreements
+.agreements-container
 {
-  margin-top: 64px;
-  // background-color: #ffffff;
+  padding-bottom: 100px;
+}
+.title-page
+{
+    font-size: 44px;
+
+    padding-top: 100px;
+    padding-bottom: 100px;
+
+    text-align: center;
+    letter-spacing: 2px;
+
+    color: #111111;
 }
 </style>
--- pages/_lang/project/index.vue
+++ pages/_lang/project/index.vue
@@ -1,14 +1,10 @@
+<!-- eslint-disable vue/max-attributes-per-line -->
 <template>
-  <v-layout
-    row
-    wrap
-    class="container-project"
-  >
-    <v-flex
-      lg6
-      offset-lg3
-    >
-      O projekcie
+  <v-layout row wrap class="contact-container">
+    <v-flex xs12 lg4 offset-lg4>
+      <div class="title-page">
+        O projekcie
+      </div>
     </v-flex>
   </v-layout>
 </template>
@@ -39,5 +35,17 @@ export default {
 {
   margin-top: 64px;
   // background-color: #ffffff;
+}
+.title-page
+{
+    font-size: 44px;
+
+    padding-top: 100px;
+    padding-bottom: 100px;
+
+    text-align: center;
+    letter-spacing: 2px;
+
+    color: #111111;
 }
 </style>
