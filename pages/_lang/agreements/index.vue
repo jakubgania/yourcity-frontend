9312b0a599f2fbf7e395ad240365331887c57293
jakubgania <jakubgania@outlook.com>
2018-11-30 11:06:13 +0100

    update meta tags

--- pages/_lang/agreements/index.vue
+++ pages/_lang/agreements/index.vue
@@ -64,7 +64,7 @@ export default {
         {
           hid: 'description',
           name: 'description',
-          content: '',
+          content: this.$t('agreements.head.content'),
         },
         {
           name: 'keywords',
