1410db6f94578d530421ca1b4e7216cd7ade4ccf
jakubgania <jakubgania@outlook.com>
2018-12-19 00:14:59 +0100

    update business/references components

--- components/business/references.vue
+++ components/business/references.vue
@@ -1,15 +1,36 @@
 <!-- eslint-disable vue/max-attributes-per-line -->
+
 <template>
   <v-flex xs12 lg12>
     <v-layout row wrap class="pages-references-section">
-      <v-flex xs10 offset-xs1 lg4 offset-lg1 class="button-link-section">
+      <v-flex
+        xs10
+        offset-xs1
+        sm8
+        offset-sm2
+        md6
+        offset-md3
+        lg4
+        offset-lg1
+        class="button-link-section"
+      >
         <router-link :to="$i18n.path('example-profile')">
           <button type="button" name="button" class="button-link">
             Zobacz przykadowy profil
           </button>
         </router-link>
       </v-flex>
-      <v-flex xs10 offset-xs1 lg4 offset-lg2 class="button-link-section">
+      <v-flex
+        xs10
+        offset-xs1
+        sm8
+        offset-sm2
+        md6
+        offset-md3
+        lg4
+        offset-lg2
+        class="button-link-section"
+      >
         <button type="button" name="button" class="button-link" @click="dialog = true">
           Poinformuj o starcie platformy
         </button>
@@ -44,6 +65,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$yourcity-blue-color: #252bfc;
+
 .pages-references-section
 {
   background-color: #f2f2f2;
@@ -58,8 +81,8 @@ export default {
   width: 100%;
   height: 60px;
   line-height: 60px;
-  border: 2px solid #252bfc;
-  color: #252bfc;
+  border: 2px solid $yourcity-blue-color;
+  color: $yourcity-blue-color;
   outline: none;
   border-radius: 4px;
   font-weight: 700;
@@ -68,8 +91,32 @@ export default {
 
   &:hover
   {
-    color: white;
-    background-color: #252bfc;
+    color: #ffffff;
+    background-color: $yourcity-blue-color;
+  }
+}
+@media only screen and (max-width: 1264px)
+{
+  .button-link-section
+  {
+    margin-top: 80px;
+    margin-bottom: 80px;
+  }
+}
+@media only screen and (max-width: 959px)
+{
+  .button-link-section
+  {
+    margin-top: 60px;
+    margin-bottom: 60px;
+  }
+}
+@media only screen and (max-width: 600px)
+{
+  .button-link-section
+  {
+    margin-top: 60px;
+    margin-bottom: 60px;
   }
 }
 </style>
