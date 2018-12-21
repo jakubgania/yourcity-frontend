44b3040d4ef97ac38d421a5c6184b6dda0e07dd6
jakubgania <jakubgania@outlook.com>
2018-12-21 23:26:12 +0100

    implemented error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,26 @@
+<!-- eslint-disable vue/max-attributes-per-line -->
+
 <template>
-  <div class="">
-    Error page
-  </div>
+  <v-layout row wrap class="error-container">
+    <v-flex lg12 style="text-align:center;">
+      <div class="message">
+        Page not found.
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
+
+<style lang="scss" scoped>
+.error-container
+{
+  width: 100%;
+  min-height: calc(100vh - 64px);
+  line-height: calc(100vh - 64px);
+}
+.message
+{
+  font-size: 20px;
+  font-weight: 800;
+  letter-spacing: 2px;
+}
+</style>
