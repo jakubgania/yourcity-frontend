c9e5943e61ff974521587387c390481e5735fd9c
jakubgania <jakubgania@outlook.com>
2018-12-31 18:16:01 +0100

    add new Google Analytics ID in ga.js"

--- plugins/ga.js
+++ plugins/ga.js
@@ -15,7 +15,7 @@ export default ({ app }) => {
   /*
   ** Set the current page
   */
-  ga('create', 'UA-128645090-1', 'auto')
+  ga('create', 'UA-131544780-2', 'auto')
   /*
   ** Every time the route changes (fired on initialization too)
   */
