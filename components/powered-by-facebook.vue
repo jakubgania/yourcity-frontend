f3bd08363a1b7351d4e848454ce7ef4906a0bb74
jakubgania <jakubgania@outlook.com>
2019-01-01 21:44:36 +0100

    update facebook logo component

--- components/powered-by-facebook.vue
+++ components/powered-by-facebook.vue
@@ -11,18 +11,18 @@
 
 <style lang="scss" scoped>
 .facebook-dialog-text {
-    display: inline;
-    margin-left: 15px;
-    font-size: 12px;
+  display: inline;
+  margin-left: 15px;
+  font-size: 12px;
 }
 .powered-by-text {
-    display: inline;
+  display: inline;
 }
 .facebook-text {
-    display: inline;
-    font-size: 12px;
-    font-weight: bold;
-    letter-spacing: 1px;
-    color: #3B5998;
+  display: inline;
+  font-size: 12px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  color: #3B5998;
 }
 </style>
