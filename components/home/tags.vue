1c28668a2894510090dbba26bef2430192216b1b
jakubgania <jakubgania@outlook.com>
2019-01-03 18:15:40 +0100

    update tags component on home page

--- components/home/tags.vue
+++ components/home/tags.vue
@@ -47,34 +47,24 @@
           </v-flex>
         </v-layout>
       </v-flex>
-      <v-flex xs10 offset-xs1 sm8 offset-sm2 lg4 offset-lg4 xl2 offset-xl5>
-        <router-link :to="$i18n.path('search')" class="link-section">
-          <v-btn
-            block
-            large
-            depressed
-            class="button"
-            data-aos="fade-right"
-            data-aos-delay="240"
-            data-aos-once="true"
-          >
-            {{ $t('home.popular_tags.search_button') }}
-            <v-icon right dark>
-              keyboard_arrow_right
-            </v-icon>
-          </v-btn>
-        </router-link>
-      </v-flex>
+
+      <button-section-component
+        :route-path="$i18n.path('search')"
+        :button-title="$t('home.popular_tags.search_button')"
+      />
+
     </v-layout>
   </v-flex>
 </template>
 
 <script>
 import TitleComponent from './title-section.vue';
+import ButtonComponent from './button-section.vue';
 
 export default {
   components: {
     'title-section-component': TitleComponent,
+    'button-section-component': ButtonComponent,
   },
   data() {
     return {
