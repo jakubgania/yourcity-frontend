5db4181b9019fd20bf965ceb9c0e0b7f2e600a32
jakubgania <jakubgania@outlook.com>
2019-04-06 23:44:35 +0200

    update powered-by-facebook.vue component

--- components/powered-by-facebook.vue
+++ components/powered-by-facebook.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="facebook-dialog-text">
-    <div class="powered-by-text">
-      powered by
-    </div>
-    <div class="facebook-text">
-      FACEBOOK
-    </div>
-  </div>
+	<div class="facebook-dialog-text">
+		<div class="powered-by-text">
+			powered by
+		</div>
+		<div class="facebook-text">
+			FACEBOOK
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
-.facebook-dialog-text {
+.facebook-dialog-text
+{
   display: inline;
   margin-left: 15px;
   font-size: 12px;
 }
-.powered-by-text {
+.powered-by-text
+{
   display: inline;
 }
-.facebook-text {
+.facebook-text
+{
   display: inline;
   font-size: 12px;
   font-weight: bold;
