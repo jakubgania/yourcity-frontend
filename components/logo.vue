82069c7447a3f6288dd2b65ee3a3313af1ac504c
jakubgania <jakubgania@outlook.com>
2019-04-07 11:19:07 +0200

    update logo.vue

--- components/logo.vue
+++ components/logo.vue
@@ -1,40 +1,35 @@
 <template>
-  <router-link :to="$i18n.path('')" class="logo-link">
-    <img
-      :style="{ marginTop: margin + 'px' }"
-      :width="logoSize"
-      :src="logo"
-      alt="Yourcity logo"
-      height="auto"
-    >
-  </router-link>
+	<router-link :to="$i18n.path('')" class="logo-link">
+		<img :style="{marginTop: margin + 'px'}" :width="logoSize" :src="logo" alt="Yourcity logo" height="auto">
+	</router-link>
 </template>
 
 <script>
 import logo from '../static/logo.svg';
 
 export default {
-  props: {
-    logoSize: {
-      type: Number,
-      default: 160,
-    },
-    margin: {
-      type: Number,
-      default: 0,
-    },
-  },
-  data() {
-    return {
-      logo,
-    };
-  },
-};
+	props: {
+		logoSize: {
+			type: Number,
+			default: 160,
+		},
+		margin: {
+			type: Number,
+			default: 0,
+		}
+	},
+	data() {
+		return {
+			logo,
+		}
+	},
+}
 </script>
 
 <style lang="scss" scoped>
 .logo-link
 {
-  text-decoration: none;
+	text-decoration: none;
 }
 </style>
+
