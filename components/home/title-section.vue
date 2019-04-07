5243141137e6aafaa43eb75c8954632451bbbcf7
jakubgania <jakubgania@outlook.com>
2019-04-07 18:29:07 +0200

    update title-section.vue

--- components/home/title-section.vue
+++ components/home/title-section.vue
@@ -1,25 +1,20 @@
 <template>
-  <v-flex xs10 offset-xs1 lg11 offset-lg1>
-    <div
-      class="title-section"
-      data-aos="fade-right"
-      data-aos-delay="240"
-      data-aos-once="true"
-    >
-      {{ title }}
-    </div>
-  </v-flex>
+	<v-flex xs10 offset-xs1 lg11 offset-lg1>
+		<div class="title-section" data-aos="fade-right" data-aos-delay="240" data-aos-once="true">
+			{{ title }}
+		</div>
+	</v-flex>
 </template>
 
 <script>
 export default {
-  props: {
-    title: {
-      type: String,
-      default: '',
-    },
-  },
-};
+	props: {
+		title: {
+			type: String,
+			default: '',
+		}
+	}
+}
 </script>
 
 <style lang="scss" scoped>
@@ -36,3 +31,4 @@ export default {
   color: #000;
 }
 </style>
+
