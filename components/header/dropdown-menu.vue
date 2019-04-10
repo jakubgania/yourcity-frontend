312db05a6738fb19e8a82b47457f0812ece84023
jakubgania <jakubgania@outlook.com>
2019-04-10 23:08:19 +0200

    update dropdown-menu.vue

--- components/header/dropdown-menu.vue
+++ components/header/dropdown-menu.vue
@@ -1,82 +1,98 @@
 <template>
-  <v-menu :nudge-width="140">
-    <v-btn slot="activator" icon>
-      <v-icon>more_vert</v-icon>
-    </v-btn>
+	<v-menu :nudge-width="140">
+		<v-btn slot="activator" icon>
+			<v-icon>
+				more_vert
+			</v-icon>
+		</v-btn>
 
-    <v-card class="card-menu">
+		<v-card class="card-menu">
+			<v-list-tile class="title-header-menu">
+				<v-list-tile-title class="text-title">
+					<v-icon class="icon">
+						translate
+					</v-icon>
+					{{ $t('header.dropdown-menu.title') }}
+				</v-list-tile-title>
+			</v-list-tile>
 
-      <v-list-tile class="title-header-menu">
-        <v-list-tile-title class="text-title">
-          <v-icon class="icon">translate</v-icon>
-          {{ $t('header.dropdown-menu.title') }}
-        </v-list-tile-title>
-      </v-list-tile>
+			<v-list-tile
+				v-for="language in languages"
+				:key="language.id"
+				class="link-section"
+				@click="changeLanguage(language.shortName)"
+			>
+				<v-list-tile-title class="text-title">
+					<v-icon class="icon">
+						chevron_right
+					</v-icon>
+					{{ language.name }}
+				</v-list-tile-title>
+			</v-list-tile>
 
-      <v-list-tile class="link-section" @click="changeLanguage('pl')">
-        <v-list-tile-title class="text-title">
-          <v-icon class="icon">chevron_right</v-icon>
-          Polski
-        </v-list-tile-title>
-      </v-list-tile>
-
-      <v-list-tile class="link-section" @click="changeLanguage('en')">
-        <v-list-tile-title class="text-title">
-          <v-icon class="icon">chevron_right</v-icon>
-          English
-        </v-list-tile-title>
-      </v-list-tile>
-
-      <v-list-tile class="link-section" @click="changeLanguage('de')">
-        <v-list-tile-title class="text-title">
-          <v-icon class="icon">chevron_right</v-icon>
-          Deutsch
-        </v-list-tile-title>
-      </v-list-tile>
-    </v-card>
-
-  </v-menu>
+		</v-card>
+	</v-menu>
 </template>
 
 <script>
 export default {
-  methods: {
-    changeLanguage(language) {
-      const routeName = this.$route.name;
+	data() {
+		return {
+			languages: [
+				{
+					name: 'Polski',
+					shortName: 'pl'
+				},
+				{
+					name: 'English',
+					shortName: 'en'
+				},
+				{
+					name: 'Deutsch',
+					shortName: 'de'
+				}
+			]
+		}
+	},
+	methods: {
+		changeLanguage(language) {
+			const routeName = this.$route.name;
 
-      if (routeName === 'index' && this.$i18n.locale !== language) {
-        this.$router.push({ name: 'lang', params: { lang: language } });
-        return;
-      }
+      		if (routeName === 'index' && this.$i18n.locale !== language) {
+        		this.$router.push({ name: 'lang', params: { lang: language } });
+        		return;
+      		}
 
-      if (routeName === 'lang' && this.$i18n.locale !== language) {
-        this.$router.push({ name: 'lang', params: { lang: language } });
-        return;
-      }
+      		if (routeName === 'lang' && this.$i18n.locale !== language) {
+        		this.$router.push({ name: 'lang', params: { lang: language } });
+        		return;
+      		}
 
-      let route = '';
+      		let route = '';
 
-      if (this.$i18n.locale === 'en') {
-        route = `lang-${this.$route.name}`;
-      } else {
-        route = this.$route.name;
-      }
+      		if (this.$i18n.locale === 'en') {
+        		route = `lang-${this.$route.name}`;
+      		} else {
+        		route = this.$route.name;
+      		}
 
-      if (this.$i18n.locale !== 'pl' && language === 'pl') {
-        this.$i18n.locale = 'pl';
-        this.$router.push({ name: route, params: { lang: 'pl' } });
-      } else if (this.$i18n.locale !== 'en' && language === 'en') {
-        this.$i18n.locale = 'en';
-        this.$router.push({ name: route, params: { lang: 'en' } });
-      } else if (this.$i18n.locale !== 'de' && language === 'de') {
-        this.$i18n.locale = 'de';
-        this.$router.push({ name: route, params: { lang: 'de' } });
-      }
-    },
-  },
-};
+      		if (this.$i18n.locale !== 'pl' && language === 'pl') {
+        		this.$i18n.locale = 'pl';
+        		this.$router.push({ name: route, params: { lang: 'pl' } });
+      		} else if (this.$i18n.locale !== 'en' && language === 'en') {
+        		this.$i18n.locale = 'en';
+        		this.$router.push({ name: route, params: { lang: 'en' } });
+      		} else if (this.$i18n.locale !== 'de' && language === 'de') {
+        		this.$i18n.locale = 'de';
+        		this.$router.push({ name: route, params: { lang: 'de' } });
+      		}
+		}
+	}
+}
 </script>
 
 <style lang="scss" scoped>
-  @import '../../assets/scss/header/dropdown-menu.scss';
+	@import '../../assets/scss/header/dropdown-menu.scss';
 </style>
+
+
