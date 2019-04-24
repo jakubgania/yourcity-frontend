9c91ff7193e7fd5640a943488032a2b3d574ceab
jakubgania <contact@jakubgania.io>
2019-04-24 23:00:51 +0200

    update network-connection-error-alert.vue

--- components/network-connection-error-alert.vue
+++ components/network-connection-error-alert.vue
@@ -3,7 +3,7 @@
 		<v-layout v-if="showErrorConnectionAlert" row wrap>
 			<v-flex :style="{marginTop: margin + 'px'}" xs10 offset-xs1 sm6 offset-sm3 lg4 offset-lg4>
 				<v-alert value="true" color="error" type="error" style="letter-spacing: 1px; font-weight: 700;">
-					{{ $t('network-connection-error-alert') }}
+					{{ $t('alerts.network-connection-error-alert') }}
 				</v-alert>
 			</v-flex>
 		</v-layout>
